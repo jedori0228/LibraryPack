7db0943f960fcfffd7d8734bac0cd8e461bfb81b
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2022-07-20 09:10:28 -0500

    sbnstyle needs more updates

--- PythonLibraries/sbnstyle.py
+++ PythonLibraries/sbnstyle.py
@@ -3,6 +3,11 @@ import ROOT as rt
 def setSBNStyle():
 
   sbnStyle =  rt.TStyle("sbnStyle","Style for SBN")
+
+  sbnStyle.SetCanvasBorderMode(0)
+  sbnStyle.SetCanvasColor(rt.kWhite)
+
   sbnStyle.SetOptStat(0)
+
   sbnStyle.cd()
 
