e5fb1b9630992170d40c1a17e656dd0fe63ac398
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2022-09-25 20:45:40 -0500

    Printing useful infos in InTimeCosmicSpillCalculator.py

--- PythonLibraries/InTimeCosmicSpillCalculator.py
+++ PythonLibraries/InTimeCosmicSpillCalculator.py
@@ -12,6 +12,9 @@ class InTimeCosmicSpillCalculator:
     self.Data_POT = Data_POT
     self.Data_Spill = Data_Spill
     self.Data_Intensity = self.Data_POT/self.Data_Spill
+    print("[mylib.__init__] Input data file POT = %1.2e"%(self.Data_POT))
+    print("[mylib.__init__] Input data file Spill = %1.2e"%(self.Data_Spill))
+    print("[mylib.__init__] Input data file Intensity = %1.2e POT/spill"%(self.Data_Intensity))
 
   def GetInTimeCosmicSpill(self, f_BeamMC, f_IntimeCosmicMC):
 
@@ -24,6 +27,10 @@ class InTimeCosmicSpillCalculator:
 
     self.BeamMC_POT = f_BeamMC.Get("BeamInfo/POT_BeamInfo").GetBinContent(1)
     self.BeamMC_Spill = f_BeamMC.Get("BeamInfo/Livetime_BeamInfo").GetBinContent(1)
+
+    print("[mylib.GetInTimeCosmicScaleFactor] BeamMC contains %1.2e POTs"%(self.BeamMC_POT))
+    print("[mylib.GetInTimeCosmicScaleFactor] BeamMC contains %1.2e spills"%(self.BeamMC_Spill))
+    print("[mylib.GetInTimeCosmicScaleFactor] -> BeamMC intensity was set to %1.2e"%(self.BeamMC_POT/self.BeamMC_Spill))
 
     ## Scale factor for the BeamMC to match the data POT
 
