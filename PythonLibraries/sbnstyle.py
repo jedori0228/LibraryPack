17d241419776c5d0f9a380df9c1479be2473a86c
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-02-09 21:29:55 -0600

    Adding Draw_ICARUS_Preliminary

--- PythonLibraries/sbnstyle.py
+++ PythonLibraries/sbnstyle.py
@@ -1,13 +1,21 @@
-import ROOT as rt
+import ROOT
 
 def setSBNStyle():
 
-  sbnStyle =  rt.TStyle("sbnStyle","Style for SBN")
+  sbnStyle =  ROOT.TStyle("sbnStyle","Style for SBN")
 
   sbnStyle.SetCanvasBorderMode(0)
-  sbnStyle.SetCanvasColor(rt.kWhite)
+  sbnStyle.SetCanvasColor(ROOT.kWhite)
 
   sbnStyle.SetOptStat(0)
 
   sbnStyle.cd()
+
+def Draw_ICARUS_Preliminary():
+
+  latex_ICARUS = ROOT.TLatex()
+  latex_ICARUS.SetNDC()
+  latex_ICARUS.SetTextSize(0.035)
+
+  latex_ICARUS.DrawLatex(0.18, 0.96, "ICARUS #it{#scale[0.8]{Preliminary}}")
 
