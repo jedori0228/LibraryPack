91536a84371e50dc4ca2cee2f29f037bbfbe0989
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-06-11 10:44:48 -0500

    Adding RecoOnly param, and Norm syst

--- PythonLibraries/SystInfo.py
+++ PythonLibraries/SystInfo.py
@@ -1,9 +1,11 @@
 class SystInfo:
 
-  def __init__(self, Name, Latex, Type, NUniv=-1):
+  def __init__(self, Name, Latex, Type, NUniv=-1, RecoOnly=False, OneSig=-1):
 
     self.Name = Name
     self.Latex = Latex
     self.Type = Type
     self.NUniv = NUniv
+    self.RecoOnly = RecoOnly
+    self.OneSig = OneSig
 
