d810b9b819a528262dc0179d223d4bb87531bfb3
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-06-11 10:45:02 -0500

    Updadting ICARUS POTs

--- PythonLibraries/ICARUSNuMIDataPOT.py
+++ PythonLibraries/ICARUSNuMIDataPOT.py
@@ -22,10 +22,12 @@ def GetICARUSNuMIPOT(run, beam, prodname):
 
     elif prodname=="DMCP2023F_F":
       #return 50555533.354061E12
-      return 5.165487279415409e+19 ## BRUCE
+      #return 5.165487279415409e+19 ## BRUCE
+      return 4.651603646822754e+19 ## JAESUNG
     elif prodname=="DMCP2023F_F_Random15Percent":
       #return 6824749.08755274E12
-      return 6.983230011348827e+18 ## BRUCE
+      #return 6.983230011348827e+18 ## BRUCE
+      return 6.983230011348827e+18 ## JAESUNG
 
     else:
       raise ValueError("[GetICARUSNuMIPOT] Wrong prodname %s for run %s"%(prodname, run))
@@ -41,17 +43,18 @@ def GetICARUSNuMIPOT(run, beam, prodname):
       return 26665149.8459446E12
 
     elif prodname=="DMCP2023F_F":
-      return 203869890.211324E12
+      #return 203869890.211324E12 ## BRUCE
+      return 1.9571570832099764e+20 ## JAESUNG
     elif prodname=="DMCP2023F_F_Random15Percent":
       #return 29182931.4684785E12 ### ARG commented out at 2024 Feb. 12th, we were using this number for technote plot
-      return 2.9300301931345543e+19 ## BRUCE
+      #return 2.9300301931345543e+19 ## BRUCE
+      return 2.9300301931345543e+19 ## JAESUNG
 
     else:
       raise ValueError("[GetICARUSNuMIPOT] Wrong prodname %s for run %s"%(prodname, run))
 
-  elif run=="Run12":
-
-    return 5.165487279415409e+19+203869890.211324E12
+  else:
+    raise ValueError("[GetICARUSNuMIPOT] Wrong run %s"%(run))
 
 
 def GetICARUSNuMILivetime(run, beam, prodname):
@@ -87,16 +90,22 @@ def GetICARUSNuMILivetime(run, beam, prodname):
 
     elif prodname=="DMCP2023F_F":
       if beam==0:
-        return 8492620.855000
+        #return 8492620.855000
+        #return 8347083.895 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 8347083.895000001 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set (comment: same as above)
       else:
         #return 9811811.313333
-        return 10755359.373333331 ## BRUCE
+        #return 10755359.373333331 ## BRUCE
+        #return 9712335.74 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 9712335.74 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set (comment: same as above)
     elif prodname=="DMCP2023F_F_Random15Percent":
       if beam==0:
         raise ValueError("[GetICARUSNuMILivetime] Random15Percent on OffBeam is wrong")
       else:
         #return 1317743.465
-        return 1453270.9883333335 ## BRUCE
+        #return 1453270.9883333335 ## BRUCE
+        #return 1453270.9883333333 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 1453270.9883333333 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set (comment: same as above)
 
     else:
       raise ValueError("[GetICARUSNuMILivetime] Wrong prodname %s for run %s"%(prodname, run))
@@ -122,15 +131,21 @@ def GetICARUSNuMILivetime(run, beam, prodname):
 
     elif prodname=="DMCP2023F_F":
       if beam==0:
-        return 44302293.065000
+        #return 44302293.065000
+        #return 42294414.57999999 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 42231883.965 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set
       else:
-        return 41194567.000000
+        #return 41194567.000000
+        #return 41328565.04666669 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 41209782.31333332 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set
     elif prodname=="DMCP2023F_F_Random15Percent":
       if beam==0:
         raise ValueError("[GetICARUSNuMILivetime] Random15Percent on OffBeam is wrong")
       else:
         #return 5883757.69333333
-        return 6.145486905e+6 ## BRUCE
+        #return 6.145486905e+6 ## BRUCE
+        #return 6145486.905 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240430_InitialBadSpillOnlyRun2 bad spill set
+        return 6141553.965 ## JAESUNG with 1) 05/07/2024 GRL + 2) 240506_UpdatedBadSpills bad spill set
 
 
     else:
